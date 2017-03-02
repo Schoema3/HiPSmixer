722a39d6ccf841f64836a94eb437a7fc40a2c15f
Schoema3 <Schoema3@tu-cottbus.de>
2017-03-02 19:07:05 +0100

    Added the flat eddy event swap. Not compiled or tested within this code.

--- include/tree.h
+++ include/tree.h
@@ -1,4 +1,7 @@
-	#include "cell.h"
+#ifndef TREE_H
+#define TREE_H
+
+#include "cell.h"
 
 class Tree {
 
@@ -14,6 +17,21 @@ public:
 
 	void createEddyEvent();
 
+	/**
+	* Randomly select a node and it's grandchildren to swap there subtrees.
+	* To identify the subtrees use the binary tree rule:
+	* The children of a node with index N are given by (2*N)+1 and 2*(N+1)
+	*/
+	void createFlatEddyEvent();
+
+private:
+	/**
+	* Exchange the source and  destination leave
+	* @ param source, index of the source leave
+	* @ param dest  , index of the source leave
+	* @ param count,  number of exchange
+	*/
+	void swapLeaves(size_t source, size_t dest, size_t count);
+
 };
-
-
+#endif
--- src/tree.cpp
+++ src/tree.cpp
@@ -1,5 +1,8 @@
 #include "tree.h"
 
+#include <cstdlib>	// rand()
+#include <cmath>	// pow(), log2()
+#include <time.h>	// time 
 Tree::Tree(double number_of_dimensions, std::vector< Cell* >* field){
 
 	std::cout << "\nCreating Binary tree...!" << std::endl;
@@ -22,4 +25,69 @@ void Tree::createEddyEvent(){
 	temp_leave = leaves->at(r1);
 	leaves->at(r1) = leaves->at(r2);
 	leaves->at(r2) = temp_leave;
+}
+
+void Tree::createFlatEddyEvent(){
+	// little note, maybe a TODO for the future
+	// get ride of the log2() and pow() because they are slow
+	// store these within the nodes.
+
+	const size_t max_valid_Grandparent_ID = pow(2,number_of_levels-2)-1;
+
+	// select random grandparent
+	const size_t grandparent_ID = rand()% max_valid_Grandparent_ID;
+
+	// select random children
+	const size_t L = rand()%2;
+	const size_t R = rand()%2;
+	
+	
+	_DEBUG(
+		std::cout << "number_of_levels         : " 		<< number_of_levels 		<< std::endl;
+		std::cout << "max_valid_Grandparent_ID : [0 - " << max_valid_Grandparent_ID <<"]"<< std::endl;
+		std::cout << "grandparent_ID           : " 		<< grandparent_ID 			<< std::endl;
+		);
+
+	// from grandparent to child ID:
+	const size_t left_child_ID  = L==0 ?  grandparent_ID*4+3 : grandparent_ID*4+4;
+	const size_t right_child_ID = R==0 ?  grandparent_ID*4+5 : grandparent_ID*4+6;
+
+	_DEBUG(
+		std::cout << std::endl;
+		std::cout << "L|R                                 : " << L <<" | "<< R << std::endl;
+		std::cout << "left_child_ID|right_child_ID        : " << left_child_ID << " | " << right_child_ID << std::endl;
+		std::cout << "child_level                         : " << child_level << std::endl;
+		);
+
+	const size_t grandparent_level = log2(grandparent_ID+1);
+	const size_t child_level = grandparent_level + 2;
+	const size_t level_2_leaves = number_of_levels - child_level;
+
+	// for the future, i guess instead of 2 use the number of children
+	// leave = startID *  2 ^lvlToGo +( 2^lvlToGo -1)
+		// to save time store the result of power
+	const size_t t = pow( 2, level_2_leaves-1);
+	const size_t source = left_child_ID  *t + t-1;
+	const size_t dest   = right_child_ID *t + t-1;
+
+	const size_t number_swaps = pow(2, number_of_levels-2  - grandparent_level -1 );
+
+	_DEBUG(
+		std::cout << std::endl;
+		std::cout << "lv to go: " << level_2_leaves << std::endl;
+		std::cout << "source  : " << source << std::endl;
+		std::cout << "dest    : " << dest << std::endl;
+		std::cout << "number_swaps  : " << number_swaps << std::endl;		
+		);
+
+	swapLeaves(source, dest, number_swaps);
+}
+
+void Tree::swapLeaves(size_t source, size_t dest, size_t count){
+	Cell* temp_leave;
+	for (int i = 0; i < count; ++i){
+		temp_leave             = leaves->at(source +i);
+		leaves->at(source +i)  = leaves->at(dest   +i);
+		leaves->at(dest   +i)  = temp_leave;
+	}
 }
